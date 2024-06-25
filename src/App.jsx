4ac0aa2843b747cc100d0aa8bfcc90d0ca44b72c
sunny <dance@kakao.com>
2024-06-25 14:16:28 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={"/image1.jpg"} alt="header" />
+        <img src={"/image2.jpg"}  alt="body1" />
+        <img src={"/image3.jpg"} alt="body2" />
+        <img src={"/image4.jpg"}  alt="body3" />
+      </div>
+    </>
+  )
+}
+
+export default App
