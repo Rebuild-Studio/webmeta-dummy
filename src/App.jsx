541725f04fa4422ff801e753c25240d9b8b90797
junman <junman9595@gmail.com>
2024-06-25 16:29:58 +0900

    feat: postMessage 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
+import dragEnd from './utils/onDragEnd'
 
 function App() {
+  
 
   return (
-    <>
-      <div>
+      <div onWheel={dragEnd}>
         <img src={"/image1.jpg"} alt="header" />
         <img src={"/image2.jpg"}  alt="body1" />
         <img src={"/image3.jpg"} alt="body2" />
         <img src={"/image4.jpg"}  alt="body3" />
       </div>
-    </>
   )
 }
 
